70415678a7dc692abd0c5ed4fb920a302165b862
POORNAVISWANATH <727722euec125@skcet.ac.in>
2026-08-19 14:44:39 +0530

    Added a MethodArgumentNotValidException handler, to handle the exception accuring during the Validation.

--- src/main/java/com/socialmediaplatform/socialmedia_app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/socialmediaplatform/socialmedia_app/exception/GlobalExceptionHandler.java
@@ -2,9 +2,12 @@ package com.socialmediaplatform.socialmedia_app.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -21,5 +24,16 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(Map.of("Error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
